e78243484f2e26ca7645ebc61b380b86d935b1a8
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2024-04-17 13:26:03 +0700

    update

--- test/webcam.py
+++ test/webcam.py
@@ -3,7 +3,7 @@ import cv2
 
 app = Flask(__name__)
 
-def generate_frames():
+def gen_frames():
     camera = cv2.VideoCapture(0)
     while True:
         success, frame = camera.read()
@@ -21,8 +21,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
+    app.run(host='0.0.0.0', port=8080, debug=True)
